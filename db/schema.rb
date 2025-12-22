9b70da5287b39d6b7b13b0528a7d4fe5f71ecec4
veselchak24 <veselchak24@bk.ru>
2025-12-23 00:14:55 +0300

    fix view

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_17_170059) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
+    t.text "content"
     t.datetime "deleted_at"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
@@ -70,15 +71,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_17_170059) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
